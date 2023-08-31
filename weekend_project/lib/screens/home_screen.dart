7c0b33b3ffa610b122c318f68e3a5a88432874d6
asma <asma.alkathiiri@gmail.com>
2023-08-31 21:11:48 +0300

    update on home screen

--- weekend_project/lib/screens/home_screen.dart
+++ weekend_project/lib/screens/home_screen.dart
@@ -15,26 +15,27 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Container(
         padding: const EdgeInsets.all(24),
-        child: Column(
-          children: [
-            const SearchTextFild(),
-            const SizedBox(height: 40),
-            GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                mainAxisExtent: 240,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SearchTextFild(),
+              const SizedBox(height: 40),
+              GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  mainAxisExtent: 240,
+                ),
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  return const MoveCard();
+                },
               ),
-              itemCount: 6,
-              itemBuilder: (context, index) {
-                return const MoveCard();
-              },
-            ),
-            const MoveCard(),
-          ],
+            ],
+          ),
         ),
       ),
     );
