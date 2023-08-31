bc574b7e657e12d0663dfb6c744d9ca6cc8c81d3
Wejdan <Wejdantaltaibi@gmail.com>
2023-08-31 13:20:09 +0300

    Test

--- weekend_project/lib/services/saved_media_service.dart
+++ weekend_project/lib/services/saved_media_service.dart
@@ -2,6 +2,6 @@ import 'package:weekend_project/services/supabase_initializer.dart';
 
 Future insertHistory(String mediaName) async {
   await SupabaseInitializer().supabaseClient.from('SavedMedia').insert({
-    'name_of_ media': mediaName,
+    'name_of_ media': mediaName, //////Some changes
   });
 }
