c4728f9d1063f571392d66aa80b155bf80bd8af4
Lama <lama.aldhafeer@gmail.com>
2023-09-03 09:07:34 +0300

    delete

--- weather_api-main/lib/constants/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static Color primary = Colors.blue.shade900;
-  static Color secondry = Colors.blue.shade500;
-  static Color light = Colors.lightBlue;
-  static Color blue = Colors.blue;
-}
